e4c85ac63253749552a40937b84bf959c68a680d
Zoe Bai <s.xue.bai@gmail.com>
2019-03-02 21:58:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,7 +87,7 @@ class Student
     students = DB[:conn].execute(
         "SELECT * FROM students WHERE grade = 10 LIMIT 1"
       )
-    students.map {|student| self.new_from_db(student)}
+    students.map {|student| self.new_from_db(student(0))}
   end
   
 end
