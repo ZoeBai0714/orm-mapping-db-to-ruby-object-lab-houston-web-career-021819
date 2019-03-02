14484b7adadaa0fa9fe3e480e66443d568a51003
Zoe Bai <s.xue.bai@gmail.com>
2019-03-02 21:51:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,7 @@ class Student
     students = DB[:conn].execute(
         "SELECT * FROM students WHERE grade <= 12"
       )
-    binding.pry  
+    #binding.pry  
     students.map {|student| self.new_from_db(student[0])} 
   end
   
